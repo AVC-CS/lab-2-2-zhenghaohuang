933fd1d004818ee376dcb84083360ae88ebd8256
zhenghaohuang <157772950+zhenghaohuang@users.noreply.github.com>
2024-02-21 03:16:53 +0000

    done

--- main.py
+++ main.py
@@ -4,6 +4,11 @@ def main():
     reg_rate = 18.25
     ov_rate = 27.78
 
+    overtime_hours = workhours - 40
+    overtime_wage = overtime_hours * ov_rate
+    regular_wage = reg_hours * reg_rate
+    total_wage = overtime_wage + regular_wage 
+
    ##################################################
    # Code your program here
    ##################################################
@@ -13,7 +18,7 @@ def main():
     # total_wage = regular_wage + overtime_wage
 
     print(f"Regular hours: {reg_hours} Regular Charge: {regular_wage}")
-    print(f"Overtime hours: {overtime} Overtime Charge: {overtime_wage:.2f}")
+    print(f"Overtime hours: {overtime_wage} Overtime Charge: {overtime_wage:.2f}")
     print(f"Total wage : {total_wage:.2f}")
 
    ##################################################
